5730b893b11684cec93d9ef06a3ea30083c64362
Yannick <yhueftle@gmail.com>
2017-02-08 16:59:42 -0700

    create DAO category interface and class

---
+++ src/main/java/com/ims/DAO/DICategory.java
@@ -0,0 +1,19 @@
+package com.ims.DAO;
+
+import java.util.List;
+
+public class DICategory implements DAOCategory{
+
+	@Override
+	public Category getCategory(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Category> getAllCategories() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
