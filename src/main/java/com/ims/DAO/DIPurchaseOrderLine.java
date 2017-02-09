ec0b37f7863f81aadcfe5215454f538fe1a317e0
awe <aweful@awews.com>
2017-02-09 13:22:30 -0700

    remove if statement from methods of DIPurchaseOrderLine

--- src/main/java/com/ims/DAO/DIPurchaseOrderLine.java
+++ src/main/java/com/ims/DAO/DIPurchaseOrderLine.java
@@ -10,43 +10,33 @@ public class DIPurchaseOrderLine implements DAOPurchaseOrderLine {
 	@Override
 	public void createPurchaseOrderLine(PurchaseOrder po) {
 		// TODO Auto-generated method stub
-		if (po == null) {
 	        throw new NotYetImplementedException();
-	    }
 		
 	}
 
 	@Override
 	public PurchaseOrderLine getPurchaseOrderLine(int poLineId) {
 		// TODO Auto-generated method stub
-		if (poLineId == null) {
 	        throw new NotYetImplementedException();
-	    }
-		return null;
 	}
 
 	@Override
 	public List<PurchaseOrderLine> getAllPurchaseOrderLines() {
 		// TODO Auto-generated method stub
 	        throw new NotYetImplementedException();
-		return null;
 	}
 
 	@Override
 	public void updatePurchaseOrderLine(int poLineId) {
 		// TODO Auto-generated method stub
-		if (poLineId == null) {
 	        throw new NotYetImplementedException();
-	    }
 		
 	}
 
 	@Override
 	public void deletePurchaseOrderLine(int poLineId) {
 		// TODO Auto-generated method stub
-		if (poLineId == null) {
 	        throw new NotYetImplementedException();
-	    }
 		
 	}
 
