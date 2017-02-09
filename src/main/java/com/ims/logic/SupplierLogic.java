ba5d23e4fb28e9a5eab50f1edfcd5d58ddfa8880
awe <aweful@awews.com>
2017-02-09 14:29:26 -0700

    add methods to SupplierLogic.java file

--- src/main/java/com/ims/logic/SupplierLogic.java
+++ src/main/java/com/ims/logic/SupplierLogic.java
@@ -1,11 +1,29 @@
 package com.ims.logic;
 
+import java.util.List;
+
+import com.ims.dao.DAOSupplier;
+import com.ims.dao.DISupplier;
 import com.ims.domain.Supplier;
 
-public class SupplierLogic {
+public class SupplierLogic {	
 	
-	Supplier sup = new Supplier();
-	
-	
+	DAOSupplier daoSup = new DISupplier() {
+		
+		@Override
+		public Supplier getSupplier(int id) {
+			// TODO Auto-generated method stub
+			Supplier sup = daoSup.getSupplier(id);
+			return sup;
+		}
+		
+		@Override
+		public List<Supplier> getAllSuppliers() {
+			// TODO Auto-generated method stub
+			
+			List<Supplier> supList = daoSup.getAllSuppliers();
+			return supList;
+		}
+	};
 
 }
