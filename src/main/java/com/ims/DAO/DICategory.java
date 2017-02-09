1c7f2d07b5d855154c0ce1405003bb3c67a12bf1
Yannick <yhueftle@gmail.com>
2017-02-09 13:13:49 -0700

    add NotYetImplementedException throws to methods and updated inventory query method names

--- src/main/java/com/ims/DAO/DICategory.java
+++ src/main/java/com/ims/DAO/DICategory.java
@@ -2,17 +2,19 @@ package com.ims.DAO;
 
 import java.util.List;
 
+import org.hibernate.cfg.NotYetImplementedException;
+
 public class DICategory implements DAOCategory{
 
 	@Override
 	public Category getCategory(int id) {
-		// TODO Auto-generated method stub
+		throw new NotYetImplementedException();
 		return null;
 	}
 
 	@Override
 	public List<Category> getAllCategories() {
-		// TODO Auto-generated method stub
+		throw new NotYetImplementedException();
 		return null;
 	}
 
