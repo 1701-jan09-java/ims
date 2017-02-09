8b8f2a592c37567a14102ee24f5a6ffcad0438eb
awe <aweful@awews.com>
2017-02-09 12:07:08 -0700

    add EmptyStackException to empty methods that implement the DAOPurchaseOrderLine interface

--- src/main/java/com/ims/DAO/DAOPurchaseOrderLine.java
+++ src/main/java/com/ims/DAO/DAOPurchaseOrderLine.java
@@ -1,8 +1,10 @@
 package com.ims.DAO;
 
+import java.util.List;
+
 public interface DAOPurchaseOrderLine {
 	//basic CRUD
-	void createPurchaseOrderLine();
+	void createPurchaseOrderLine(PurchaseOrder po);
 	PurchaseOrderLine getPurchaseOrderLine(int poLineId);
 	List<PurchaseOrderLine> getAllPurchaseOrderLines();
 	void updatePurchaseOrderLine(int poLineId);
--- src/main/java/com/ims/DAO/DIPurchaseOrderLine.java
+++ src/main/java/com/ims/DAO/DIPurchaseOrderLine.java
@@ -1,34 +1,50 @@
 package com.ims.DAO;
 
+import java.util.EmptyStackException;
+import java.util.List;
+
 public class DIPurchaseOrderLine implements DAOPurchaseOrderLine {
 
 	@Override
-	public void createPurchaseOrderLine() {
+	public void createPurchaseOrderLine(PurchaseOrder po) {
 		// TODO Auto-generated method stub
+		if (po == null) {
+	        throw new EmptyStackException();
+	    }
 		
 	}
 
 	@Override
 	public PurchaseOrderLine getPurchaseOrderLine(int poLineId) {
 		// TODO Auto-generated method stub
+		if (poLineId == null) {
+	        throw new EmptyStackException();
+	    }
 		return null;
 	}
 
 	@Override
 	public List<PurchaseOrderLine> getAllPurchaseOrderLines() {
 		// TODO Auto-generated method stub
+	        throw new EmptyStackException();
 		return null;
 	}
 
 	@Override
 	public void updatePurchaseOrderLine(int poLineId) {
 		// TODO Auto-generated method stub
+		if (poLineId == null) {
+	        throw new EmptyStackException();
+	    }
 		
 	}
 
 	@Override
 	public void deletePurchaseOrderLine(int poLineId) {
 		// TODO Auto-generated method stub
+		if (poLineId == null) {
+	        throw new EmptyStackException();
+	    }
 		
 	}
 
