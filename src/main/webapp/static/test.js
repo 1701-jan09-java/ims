3ece01ae0d3dd5e59ab3267a876006893e170aa0
Chris Matheny <cmatheny@asu.edu>
2017-02-11 22:08:55 -0700

    Add js for ajax calls to servlets

--- src/main/webapp/static/test.js
+++ src/main/webapp/static/test.js
@@ -1,3 +1,57 @@
-/**
- * 
- */
+var TEST = {
+    Products: {}
+};
+
+$(document).ready(function() {
+    var sendRequest = function (group,id) {
+        var tempUrl = "/ims/"+group+"/";
+        
+        if (id) {
+            tempUrl += id;
+        }
+        
+        $.ajax({
+            method: "GET",
+            url: tempUrl,
+
+            success: function(data) {
+                TEST.Products = data;
+                console.log(data);
+            },
+
+            failure: function() {
+                setTimeout(function(){productRequest(id);},2000);
+            }
+        });
+    };
+    
+    $("#prodById").click(function(){
+        console.log("hi");
+        var input = $("#idInput").val();
+        console.log(input);
+        if ($.isNumeric(input)) {
+            var id = input;
+            sendRequest("product",id);
+        } else console.log("Invalid Entry");
+    });
+    
+    $("#allProd").click(function(){
+        console.log("hi");
+        sendRequest("product");
+    });
+    
+    $("#catById").click(function(){
+        console.log("hi");
+        var input = $("#idInput").val();
+        console.log(input);
+        if ($.isNumeric(input)) {
+            var id = input;
+            sendRequest("category",id);
+        } else console.log("Invalid Entry");
+    });
+    
+    $("#allCat").click(function(){
+        console.log("hi");
+        sendRequest("category");
+    });
+});
