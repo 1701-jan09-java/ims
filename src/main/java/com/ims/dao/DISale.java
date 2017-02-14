64cff6bbead84d453fefcaec99bd480e1de1ca72
sharangw <sharang@Sharangs-MacBook-Pro.local>
2017-02-13 23:15:35 -0700

    add SaleLogic and complete DISale methods

--- src/main/java/com/ims/dao/DISale.java
+++ src/main/java/com/ims/dao/DISale.java
@@ -2,43 +2,105 @@ package com.ims.dao;
 
 import java.util.List;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.NotYetImplementedException;
 
+import com.ims.domain.Retailer;
 import com.ims.domain.Sale;
+import com.ims.domain.util.HibernateUtil;
 
 public class DISale implements DAOSale{
 
 	@Override
 	public void createSale(Sale sale) {
-		throw new NotYetImplementedException();
+		
+		Session session = HibernateUtil.getSession(); 
+		Transaction tx = session.beginTransaction();
+		
+		session.save(sale);
+		
+		tx.commit();
+		session.close();
 		
 	}
 
 	@Override
 	public void updateSale(Sale sale) {
-		throw new NotYetImplementedException();
+		
+		Session session = HibernateUtil.getSession(); 
+		Transaction tx = session.beginTransaction();
+		
+		session.update(sale);
+		
+		tx.commit();
+		session.close();
 		
 	}
 
 	@Override
 	public void deleteSale(Sale sale) {
-		throw new NotYetImplementedException();
+		
+		Session session = HibernateUtil.getSession(); 
+		Transaction tx = session.beginTransaction();
+		
+		session.delete(sale);
+		
+		tx.commit();
+		session.close();
 		
 	}
 
 	@Override
 	public Sale getSale(int id) {
-		throw new NotYetImplementedException();
+		
+		Session session = HibernateUtil.getSession(); 
+		
+		Sale sale = (Sale) session.get(Sale.class, id);
+		
+		session.close();
+		
+		return sale;
+		
 	}
 
 	@Override
 	public List<Sale> getAllSales() {
-		throw new NotYetImplementedException();
+		
+		Session session = HibernateUtil.getSession(); 
+		
+		Query query = session.createQuery("from Sale");
+		List<Sale> allSales = query.list();
+	
+		session.close();
+		
+		return allSales;
+		
 	}
 
 	@Override
 	public List<Sale> getAllSalesByRetailer(int retailerID) {
-		throw new NotYetImplementedException();
+		
+		Session session = HibernateUtil.getSession(); 
+		
+		Query query = session.createQuery("from Sale where retailer = :selectedRet");
+		query.setInteger("selectedRet", retailerID);
+		List<Sale> salesByRet = query.list();
+		
+		session.close();
+		
+		if (salesByRet.isEmpty()) {
+			
+			System.out.println("No such retailer has sales");
+			return null;
+			
+		}
+		
+		return salesByRet;
+		
+	
+		
 	}
 
 }
