cfd2a1062aa57b3d096db02d6166d6216ee591e6
awe <aweful@awews.com>
2017-02-18 22:54:47 -0700

    start adding direports

--- src/main/java/com/ims/dao/DAOReports.java
+++ src/main/java/com/ims/dao/DAOReports.java
@@ -1,5 +1,34 @@
 package com.ims.dao;
 
+import java.util.List;
+
+import com.ims.domain.Category;
+import com.ims.domain.Inventory;
+import com.ims.domain.Product;
+import com.ims.domain.Retailer;
+import com.ims.domain.Supplier;
+
 public interface DAOReports {
+	
+	//Query
+	List<Product> sellDollarsByProduct();
+	List<Product> sellQuantityByProduct();
+	
+	List<Product> avgSellDollarsByProduct();
+	List<Product> avgQuantityByProduct();
+	
+	List<Retailer> sellDollarsByRetailer();
+	List<Retailer> sellQuantityByRetailer();
+	
+	List<Supplier> sellDollarsBySupplier();
+	List<Supplier> sellQuantityBySupplier();
+	
+	List<Product> inventoryByProductByRetailer();
+	List<Product> avgInventoryByProductByRetailer();
+	
+	List<Product> inventoryThresholdByProduct();
+	List<Product> inventoryThresholdByProductByRetailer();
+	
+	List<Product> inventoryByProduct();
 
 }
---
+++ src/main/java/com/ims/dao/DIReports.java
@@ -0,0 +1,5 @@
+package com.ims.dao;
+
+public class DIReports {
+
+}
