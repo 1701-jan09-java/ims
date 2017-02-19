9645dca95cb9c8c6c35b5e422d85c73aebb07159
sharangw <sharang@Sharangs-MacBook-Pro.local>
2017-02-19 12:30:23 -0700

    changed sale view by putting data in table

--- src/main/webapp/static/scripts/salesByRetailer.js
+++ src/main/webapp/static/scripts/salesByRetailer.js
@@ -30,7 +30,40 @@ function salesByRetButton(input) {
   	 	//console.log(global.modalBody);
   	 	global.modalHeader = document.getElementById("modal-title");
   	 	
-  	 	var saleRow = document.createElement("div");
+  	 	var saleTable = document.createElement("table");
+ 	 	saleTable.classList.add("table", "table-hover", "table-bordered", "table-responsive");
+ 	 	global.modalBody.appendChild(saleTable);
+ 	 	
+ 	 	var saleTableBody = document.createElement("tbody");
+ 	 	saleTable.appendChild(saleTableBody);
+ 	 	var saleHeader = document.createElement("thead");
+ 	 	saleTable.appendChild(saleHeader);
+ 	 	
+ 	 	var saleHeadRow = document.createElement("tr");
+ 	 	saleHeader.appendChild(saleHeadRow);
+ 	 	
+ 	 	var saleIdHeader = document.createElement("th");
+ 	 	saleIdHeader.innerHTML = "Sale ID";
+ 	 	saleHeadRow.appendChild(saleIdHeader);
+ 	 	
+ 	 	var saleProductNameHeader = document.createElement("th");
+ 	 	saleProductNameHeader.innerHTML = "Product Name";
+ 	 	saleHeadRow.appendChild(saleProductNameHeader);
+ 	 	
+ 	 	var saleProductIdHeader = document.createElement("th");
+ 		saleProductIdHeader.innerHTML = "Product ID";
+ 	 	saleHeadRow.appendChild(saleProductIdHeader);
+ 	 	
+ 	 	var saleProductCostHeader = document.createElement("th");
+ 	 	saleProductCostHeader.innerHTML = "Cost";
+ 	 	saleHeadRow.appendChild(saleProductCostHeader); 		
+ 	 	
+ 	 	var saleProductQuantityHeader = document.createElement("th");
+ 	 	saleProductQuantityHeader.innerHTML = "Quantity";
+ 	 	saleHeadRow.appendChild(saleProductQuantityHeader);
+ 	 	
+ 	 	/*	 	
+ 	 	var saleRow = document.createElement("div");
   	 	saleRow.className = "row";
   	 	global.modalBody.appendChild(saleRow);
   	 	//console.log(global.modalBody);
@@ -63,7 +96,8 @@ function salesByRetButton(input) {
   	 	quantitySoldHeader.innerHTML = '<b>'+"Quantity"+'</b>';
   	 	quantitySoldHeader.className = "col-md-2";
   	 	saleHeader.appendChild(quantitySoldHeader);
-  	 	
+  	 	*/
+ 	 	
   	 	global.modalHeader.innerHTML = global.retailData[0].retailer.name;
   	 	
   	 	 for(let index = 0; index < global.retailData.length; index++){
@@ -74,6 +108,30 @@ function salesByRetButton(input) {
   	 		 let productCost = global.retailData[index].cost;
   	 		 let quantitySold = global.retailData[index].productQuantity;
   	 		 
+  	 		 var newTableRow = document.createElement("tr");
+  	 		 saleTableBody.appendChild(newTableRow);     	 		
+	 		
+  	 		 var sID = document.createElement("td");
+  	 		 sID.innerHTML = saleID;
+  	 		 newTableRow.appendChild(sID);
+  	 		 
+  	 		 var prodID = document.createElement("td");
+  	 		 prodID.innerHTML = productID;
+ 	 		 newTableRow.appendChild(prodID);
+  	 		 
+ 	 		 var prodName = document.createElement("td");
+  	 		 prodName.innerHTML = productName;
+ 	 		 newTableRow.appendChild(prodName);
+ 	 		 
+ 	 		 var prodCost = document.createElement("td");
+ 	 		 prodCost.innerHTML = productCost;
+ 	 		 newTableRow.appendChild(prodCost);
+ 	 		 
+ 	 		 var prodQuantity = document.createElement("td");
+ 	 		 prodQuantity.innerHTML = quantitySold;
+ 	 		 newTableRow.appendChild(prodQuantity);
+ 	 		 
+  	 		 /*
   	 		 var newRow = document.createElement("div");
   	 		 newRow.className="row";
   	 		 global.modalBody.appendChild(newRow);
@@ -106,7 +164,7 @@ function salesByRetButton(input) {
   	 		prodQuantity.innerHTML = quantitySold;
   	 		prodQuantity.className="col-md-2";
   	 		newInnerRow.appendChild(prodQuantity); 
- 	 		 
+ 	 		 */
   	 	 }
   	 	 
   	     $('#modal').modal('show');
